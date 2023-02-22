27d7c4d2a3c3640bc7675c717836008fd460aa99
Koverin <110396064+Koverin@users.noreply.github.com>
2023-02-22 19:22:32 +0200

    +1

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Message } from './Notification.styled';
 
 export const Notification = ({ message }) => {
   return (
     <>
-      <p>{message}</p>
+      <Message>{message}</Message>
     </>
   );
 };
